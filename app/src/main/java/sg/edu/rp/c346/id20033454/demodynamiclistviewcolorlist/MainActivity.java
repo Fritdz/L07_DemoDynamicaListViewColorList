c57ca6365f9e20c69dc864aa0d3b0a244217a647
Fritdz <hafidzzz@hotmail.sg>
2021-06-08 12:31:56 +0800

    Click Event

--- app/src/main/java/sg/edu/rp/c346/id20033454/demodynamiclistviewcolorlist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20033454/demodynamiclistviewcolorlist/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -44,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
                 alColours.add(pos, newColour);
 
                 aaColour.notifyDataSetChanged();
+            }
+        });
+
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String colour = alColours.get(position);
+                Toast.makeText(MainActivity.this, colour, Toast.LENGTH_SHORT).show();
             }
         });
 
